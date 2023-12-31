831567e0264e7bbf442e6e76203a6fe57374b9c2
SelamB <selambeyene2@gmail.com>
2023-12-30 16:16:26 -0800

    converts python to javascript

---
+++ Linked Lists/linked_list.js
@@ -0,0 +1,76 @@
+class Node {
+    constructor(data) {
+      this.data = data;
+      this.next = null;
+      this.previous = null;
+    }
+  }
+  
+  class LinkedList {
+    constructor() {
+      this.head = null;
+    }
+  
+    insert(data) {
+      const newNode = new Node(data);
+  
+      if (this.head === null) {
+        this.head = newNode;
+      } else {
+        let lastNode = this.head;
+        while (lastNode.next !== null) {
+          console.log("Traversing linked list...");
+          lastNode = lastNode.next;
+        }
+        lastNode.next = newNode;
+        newNode.previous = lastNode;
+      }
+    }
+  
+    deleteByKey(key) {
+      console.log(`\nDelete Element ${key} from Linked List`);
+      let currentNode = this.head;
+      let previousNode = null;
+  
+      if (currentNode !== null && currentNode.data === key) {
+        this.head = currentNode.next;
+        console.log(`${key} found and deleted`);
+        return;
+      }
+  
+      while (currentNode !== null && currentNode.data !== key) {
+        previousNode = currentNode;
+        currentNode = currentNode.next;
+      }
+  
+      if (currentNode !== null) {
+        previousNode.next = currentNode.next;
+        console.log(`${key} found and deleted`);
+      }
+  
+      if (currentNode === null) {
+        console.log(`${key} not found`);
+      }
+    }
+  
+    printList() {
+      let currentNode = this.head;
+      process.stdout.write("Linked List Elements: ");
+      while (currentNode !== null) {
+        process.stdout.write(`${currentNode.data} `);
+        currentNode = currentNode.next;
+      }
+      console.log();
+    }
+  }
+  
+  const linkedList = new LinkedList();
+  linkedList.insert(4);
+  linkedList.insert(1);
+  linkedList.insert(3);
+  linkedList.insert(8);
+  
+  linkedList.printList();
+  linkedList.deleteByKey(1);
+  linkedList.printList();
+  
---
+++ Linked Lists/linked_list.py
@@ -0,0 +1,64 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.previous = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert(self, data):
+        new_node = Node(data)
+        new_node.next = None
+        new_node.previous = None
+
+        if self.head is None:
+            self.head = new_node
+        else:
+            last_node = self.head
+            while last_node.next is not None:
+                print("Traversing linked list...")
+                last_node = last_node.next
+            last_node.next = new_node
+            new_node.previous = last_node
+
+    def delete_by_key(self, key):
+        print()
+        print(f"Delete Element {key} from Linked List")
+        current_node = self.head
+        previous_node = None
+
+        if current_node is not None and current_node.data == key:
+            self.head = current_node.next
+            print(f"{key} found and deleted")
+            return
+
+        while current_node is not None and current_node.data != key:
+            previous_node = current_node
+            current_node = current_node.next
+
+        if current_node is not None:
+            previous_node.next = current_node.next
+            print(f"{key} found and deleted")
+        if current_node is None:
+            print(f"{key} not found")
+
+    def print_list(self):
+        current_node = self.head
+        print("Linked List Elements: ", end="")
+        while current_node is not None:
+            print(current_node.data, end=" ")
+            current_node = current_node.next
+        print()
+
+if __name__ == "__main__":
+    linked_list = LinkedList()
+    linked_list.insert(4)
+    linked_list.insert(1)
+    linked_list.insert(3)
+    linked_list.insert(8)
+
+    linked_list.print_list()
+    linked_list.delete_by_key(1)
+    linked_list.print_list()
